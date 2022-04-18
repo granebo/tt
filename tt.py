45aada07f146a9131f87b8723903376d696f4121
Tobias <github@friman.name>
2022-04-18 21:45:52 +0200

    Removed kid.py, added a parameter to the render of login.html

--- tt.py
+++ tt.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', name = 'Kalle')
+
 
 app.run(host='0.0.0.0', port=81)
