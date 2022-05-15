4556c7b2ddd41cbafe92873159342d6018b84488
Tobias <github@friman.name>
2022-05-15 09:14:17 +0200

    Routing to kid and kids

--- tt.py
+++ tt.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def login():
     return render_template('login.html', name = 'Kalle')
+
+
+@app.route('/kids.html')
+def kids():
+    kidslist = [('Kalle','kalle'),('Lisa','lisa')]
+    return render_template('kids.html', kids = kidslist)
+
+
+
+@app.route('/kid.html')
+def kid():
+    username = request.values.get('kid')
+    return render_template('kid.html', kid = username)
+
+
+
+
+
 
 
 app.run(host='0.0.0.0', port=81)
